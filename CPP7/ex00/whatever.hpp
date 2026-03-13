ee4426f3a1ddc431f0c1e551e9b2591c4cf76568
Bastien <bozil@student.42nice.fr>
2026-03-13 14:20:13 +0100

    ex00

--- CPP7/ex00/whatever.hpp
+++ CPP7/ex00/whatever.hpp
@@ -2,9 +2,25 @@
 
 #include <iostream>
 
-class Whatever
+template <typename T> void swap(T &a, T &b)
 {
-    public:
-        Whatever;
-        ~Whatever;
-};
+	T	tmp;
+
+	tmp = a;
+	a = b;
+	b = tmp;
+}
+
+template <typename T> const T &min(const T &a, const T &b)
+{
+	if (a < b)
+		return (a);
+	return (b);
+}
+
+template <typename T> const T &max(const T &a, const T &b)
+{
+	if (a > b)
+		return (a);
+	return (b);
+}

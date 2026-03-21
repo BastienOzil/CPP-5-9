342352b528c04c916bd6a456cab12e72fa97276e
Bozil <bastien.ozil17@gmail.com>
2026-03-21 15:45:32 +0100

    cpp 8 ex02 finish

--- CPP8/ex02/MutantStack.hpp
+++ CPP8/ex02/MutantStack.hpp
@@ -1,3 +1,63 @@
 #pragma once
 
-#include <iostream>
+#include <stack>
+
+template <typename T>
+class MutantStack : public std::stack<T>
+{
+public:
+    typedef typename std::stack<T>::container_type container_type;
+    typedef typename container_type::iterator iterator;
+    typedef typename container_type::const_iterator const_iterator;
+    typedef typename container_type::reverse_iterator reverse_iterator;
+    typedef typename container_type::const_reverse_iterator const_reverse_iterator;
+
+    MutantStack() {}
+    MutantStack(const MutantStack& other) : std::stack<T>(other) {}
+    MutantStack& operator=(const MutantStack& other)
+    {
+        std::stack<T>::operator=(other);
+        return *this;
+    }
+    ~MutantStack() {}
+
+    iterator begin()
+    {
+        return std::stack<T>::c.begin();
+    }
+
+    const_iterator begin() const
+    {
+        return std::stack<T>::c.begin();
+    }
+
+    iterator end()
+    {
+        return std::stack<T>::c.end();
+    }
+
+    const_iterator end() const
+    {
+        return std::stack<T>::c.end();
+    }
+
+    reverse_iterator rbegin()
+    {
+        return std::stack<T>::c.rbegin();
+    }
+
+    const_reverse_iterator rbegin() const
+    {
+        return std::stack<T>::c.rbegin();
+    }
+
+    reverse_iterator rend()
+    {
+        return std::stack<T>::c.rend();
+    }
+
+    const_reverse_iterator rend() const
+    {
+        return std::stack<T>::c.rend();
+    }
+};

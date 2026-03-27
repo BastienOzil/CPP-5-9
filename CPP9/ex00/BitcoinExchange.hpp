68731f7d347685c8a449faedef274bb8f6889245
Bastien <bozil@student.42nice.fr>
2026-03-27 13:51:54 +0100

    well start about filestream

--- CPP9/ex00/BitcoinExchange.hpp
+++ CPP9/ex00/BitcoinExchange.hpp
@@ -1,2 +1,12 @@
+#include <fstream>
 #include <iostream>
 
+class BitcoinExchange
+{
+  private:
+  public:
+	BitcoinExchange();
+	BitcoinExchange(const BitcoinExchange &other);
+	BitcoinExchange &operator=(const BitcoinExchange &other);
+	~BitcoinExchange();
+};
--- CPP9/ex00/main.cpp
+++ CPP9/ex00/main.cpp
@@ -1,9 +1,28 @@
 #include "BitcoinExchange.hpp"
 
-int main(int argc, char **argv)
+int	main(int argc, char **argv)
 {
-    if(argc != 2)
-        std::cout << "Usage: ./btc <file name>" << std::endl;
-        return 0;
+	std::ifstream file(argv[1]);
+	if (argc == 2)
+	{
+		if (file.is_open())
+		{
+			std::cout << "File found" << std::endl;
 
+			BitcoinExchange btc;
+			return (1);
+		}
+		else
+		{
+			std::cerr << "Error: Is not a valid file name" << std::endl;
+			return (0);
+		}
+	}
+	else
+	{
+		std::cout << "Usage: ./btc <file name>" << std::endl;
+	}
+
+	file.close();
+	return (0);
 }
